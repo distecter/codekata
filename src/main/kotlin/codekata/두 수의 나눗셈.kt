c5aed67fd20caa95360284b830560a1bd7354223
distecter <152145394+distecter@users.noreply.github.com>
2023-12-22 09:27:00 +0900

    refactor : 코드카타 파일구조 리팩토링

--- src/main/kotlin/codekata/두 수의 나눗셈.kt
+++ src/main/kotlin/codekata/두 수의 나눗셈.kt
@@ -1,15 +1,15 @@
 package codekata
 
 fun main() {
-    val value = Solution()
-    val result = value.Solution(3, 2)
+    val value = `두 수의 나눗셈`()
+    val result = value.solution(3, 2)
     print(result)
 }
 
-class Solution {
-    fun Solution(num1: Int, num2: Int): Int {
+class `두 수의 나눗셈` {
+    fun solution(num1: Int, num2: Int): Int {
         var answer: Int = 0
-        answer= (num1.toDouble()/num2.toDouble()*1000).toInt()
+        answer = (num1.toDouble() / num2.toDouble() * 1000).toInt()
         return answer
     }
 }
--- src/main/kotlin/codekata/두수의 합.kt
+++ src/main/kotlin/codekata/두수의 합.kt
@@ -1,12 +1,12 @@
 package codekata
 
 fun main() {
-    val value = Solution()
-    val result = value.Solution(100, 2)
+    val value = `두 수의 합`()
+    val result = value.solution(100, 2)
     print(result)
 }
-class Solution {
-    fun Solution(num1: Int, num2: Int): Int {
+class `두 수의 합` {
+    fun solution(num1: Int, num2: Int): Int {
         var answer: Int = num1 + num2
         return answer
     }
